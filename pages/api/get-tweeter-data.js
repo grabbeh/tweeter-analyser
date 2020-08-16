186a6bceba758881502982adf9ce0f4d06dc4ac9
Michael <mbg@outlook.com>
2020-08-16 13:00:50 +0100

    Dynamo

--- pages/api/get-tweeter-data.js
+++ pages/api/get-tweeter-data.js
@@ -5,15 +5,18 @@ export default async (req, res) => {
   let username = JSON.parse(req.body).username
   let user = await getUser(username)
   let existingResults = await findItem(user.id)
-  if (existingResults) {
+  if (existingResults && existingResults.body !== '{}') {
     res.statusCode = 200
     res.json(existingResults.body)
   } else {
     let results = await tweeter(username)
     let response = await addItem(user.id, results)
+    // console.log(response)
     res.statusCode = 200
-    res.json({
-      ...results
-    })
+    res.json({ Item: results })
   }
+}
+
+const empty = obj => {
+  return Object.keys(obj).length === 0 && obj.constructor === Object
 }
