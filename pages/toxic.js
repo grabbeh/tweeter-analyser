4e39806ae0cd2b8555149f7d6f07e0efb24c3468
Michael <mbg@outlook.com>
2020-08-26 23:36:14 +0100

    Url

--- pages/toxic.js
+++ pages/toxic.js
@@ -30,7 +30,7 @@ const Toxic = ({ data }) => {
                       fontWeight: 'bold'
                     }}
                   >
-                    <Link href={`/form?username=${account.screen_name}`}>
+                    <Link href={`/search?username=${account.screen_name}`}>
                       <ThemeLink>@{account.screen_name}</ThemeLink>
                     </Link>
                   </Text>
