5febc5825abf49a134c11bb0ee4e0b9915d8cbe1
Michael <mbg@outlook.com>
2020-12-06 00:01:09 +0000

    Server.js

--- server/lockdown-bot/server.js
+++ server/lockdown-bot/server.js
@@ -1,12 +1,12 @@
 import cron from 'node-cron'
 import express from 'express'
-import tweetMentionedUser from './tweetUser.js'
+import tweetUser from './tweetUser.js'
 
 const app = express()
 
 // schedule tasks to be run on the server
 cron.schedule('* * * * *', function () {
-  tweetMentionedUser()
+  tweetUser()
 })
 
 app.listen('3128')
